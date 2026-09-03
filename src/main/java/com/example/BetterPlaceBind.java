ca388c2d6823fbc452dfa1c52902b1dae36bf737
monkeyeinmonkey1-hash <monkeyeinmonkey1@gmail.com>
2026-09-03 02:37:20 +0200

    Add files via upload

---
+++ src/main/java/com/example/BetterPlaceBind.java
@@ -0,0 +1,16 @@
+/*
+ * Decompiled with CFR 0.152.
+ * 
+ * Could not load the following classes:
+ *  net.fabricmc.api.ClientModInitializer
+ */
+package betterplacebind;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterPlaceBind
+implements ClientModInitializer {
+    public void onInitializeClient() {
+    }
+}
+
